73854eefae67c7eeaa7d4cfba1e48d76f089012f
GitHub Action <action@github.com>
2021-03-11 06:29:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/lihuu/simplecodegenerator/services/MyProjectService.kt
+++ src/main/kotlin/com/github/lihuu/simplecodegenerator/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.lihuu.simplecodegenerator.services
 
+import com.github.lihuu.simplecodegenerator.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 

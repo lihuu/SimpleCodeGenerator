c953d85c604a34e5b8f42a1d0028fa8783f1a664
lihu <lihu@eventslack.com>
2021-03-18 18:16:36 +0800

    Add GUI demo

---
+++ src/main/kotlin/MyForm.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyForm {
+    private JButton button1;
+    private JPanel panel1;
+
+    public MyForm() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
